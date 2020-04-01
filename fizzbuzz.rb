bd91e8ed1b1545ca73958f92ace14dc3884519c2
Kristina Frederick <kristinafrederick@icloud.com>
2020-04-01 22:48:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(num)
+  if num % 3 == 0
     puts "Fizz"
-  elsif int % 5 == 0
+  elsif num % 5 == 0
     puts "Buzz"
-  else int % 3 && 5 == 0
+  else num % 3 && 5 == 0
     puts "Fizzbuzz"
   end
 end
