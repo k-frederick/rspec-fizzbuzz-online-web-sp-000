d1a2c34f45aed8b8f05e00dc03045c6ea2190881
Kristina Frederick <kristinafrederick@icloud.com>
2020-04-01 22:57:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    returns "Fizzbuzz"
+    "Fizzbuzz"
   elsif num % 3 == 0
-    returns "Buzz"
+    "Buzz"
   else num % 5 == 0
-    returns "Fizz"
+    "Fizz"
   end
 end
