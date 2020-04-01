ef795e772d1ad4515195c823e45530b0e8cbe5ab
Kristina Frederick <kristinafrederick@icloud.com>
2020-04-01 22:52:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    puts "Fizz"
-  elsif num % 5 == 0
-    puts "Buzz"
-  else
     puts "Fizzbuzz"
+  elsif num % 3 == 0
+    puts "Buzz"
+  else num % 5 == 0
+    puts "Fizz"
   end
 end
