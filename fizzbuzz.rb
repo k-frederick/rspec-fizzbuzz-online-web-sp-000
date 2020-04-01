46bd6ca6dbf4ffa348fc7bb3e5eb95f419ab4a46
Kristina Frederick <kristinafrederick@icloud.com>
2020-04-01 22:26:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-
+  if 3
+    puts "Fizz"
+  end
+  elsif 5
+  puts "Buzz"
+  end
+  else 15
+  puts "fizzbuzz"
+  end
 end
